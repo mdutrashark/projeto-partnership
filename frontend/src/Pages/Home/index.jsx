811137fdfe61d8075c214db5ad396ad48dc02a67
mdutrashark <marcos.md4199@gmail.com>
2024-11-25 23:24:23 -0300

    Criados os componentes Footer e Form

--- frontend/src/Pages/Home/index.jsx
+++ frontend/src/Pages/Home/index.jsx
@@ -1,10 +1,22 @@
 import { Header } from "../../Components/Header"
+import { Footer } from "../../Components/Footer"
 
 export function Home(){
     return(
         <div>
             <Header/>
             Home
+            <br />
+            Sobre
+            <br />
+            Campanha
+            <br />
+            Experiências
+            <br />
+            Contatos
+            <br />
+            <Footer/>
+            
         </div>
     )
 }
